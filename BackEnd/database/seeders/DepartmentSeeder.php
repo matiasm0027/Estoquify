8d1ed07388040ade2f70a04fa891efe2d375a7c2
matiasm0027 <matiasm0027@gmail.com>
2024-04-04 13:23:25 +0200

    Department description

--- BackEnd/database/seeders/DepartmentSeeder.php
+++ BackEnd/database/seeders/DepartmentSeeder.php
@@ -11,13 +11,13 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        Department::create(['name' => 'computing']);
-        Department::create(['name' => 'billing']);
-        Department::create(['name' => 'accounting']);
-        Department::create(['name' => 'finance']);
-        Department::create(['name' => 'commercial']);
-        Department::create(['name' => 'sac']);
-        Department::create(['name' => 'shopping']);
-        Department::create(['name' => 'logistics']);
+        Department::create(['name' => 'Computing', 'description' => 'Department dedicated to managing and maintaining computer systems and software.']);
+        Department::create(['name' => 'Billing', 'description' => 'Department responsible for managing billing processes and invoices.']);
+        Department::create(['name' => 'Accounting', 'description' => 'Department focused on financial recordkeeping, including bookkeeping, payroll, and auditing.']);
+        Department::create(['name' => 'Finance', 'description' => 'Department responsible for managing financial resources and making strategic financial decisions.']);
+        Department::create(['name' => 'Commercial', 'description' => 'Department focused on sales, marketing, and business development activities.']);
+        Department::create(['name' => 'Sac', 'description' => 'Department dedicated to providing customer support and resolving issues and complaints.']);
+        Department::create(['name' => 'Shopping', 'description' => 'Department responsible for purchasing goods and services for the organization.']);
+        Department::create(['name' => 'Logistics', 'description' => 'Department responsible for managing the flow of goods, services, and information within the organization.']);
     }
 }
