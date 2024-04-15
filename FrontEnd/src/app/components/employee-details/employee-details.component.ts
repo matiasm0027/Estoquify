b3fd89e109bfabee096c7279121417b7f7db0bfa
Orma4705 <oriolmartincasas@gmail.com>
2024-04-15 07:58:52 -0400

    detalles_usuario

--- FrontEnd/src/app/components/employee-details/employee-details.component.ts
+++ FrontEnd/src/app/components/employee-details/employee-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiRequestService } from 'src/app/services/api/api-request.service';
 import { UsuariosControlService } from 'src/app/services/usuarios/usuarios-control.service';
@@ -13,8 +14,24 @@ export class EmployeeDetailsComponent implements OnInit {
   employeeDetails: any = {};
   sidebarVisible: boolean = true;
   sidebarWidth: number = 250;
+  departamentos: any[] = [];
+  sucursales: any[] = [];
+  mostrarModalEditar: boolean = false;
+  formularioEmpleado: FormGroup;
+ 
 
-  constructor(private route: ActivatedRoute, private employeeService: ApiRequestService, private router: Router,) { }
+  constructor(private fb: FormBuilder,private route: ActivatedRoute, private employeeService: ApiRequestService, private router: Router,) {
+    this.formularioEmpleado = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      departamento: ['', Validators.required],
+      sucursal: ['', Validators.required],
+      rol: ['', Validators.required],
+      telefonoMovil: ['', Validators.required],
+    });
+   }
 
   ngOnInit(): void {
     this.getEmployeeIdFromRoute();
@@ -50,6 +67,71 @@ export class EmployeeDetailsComponent implements OnInit {
         }
       );
 
+  }
+  volver(){
+    this.router.navigate(['/employees_view'])
+  }
+
+  mostrarModal() {
+    this.mostrarModalEditar = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModalEditar = false;
+    this.getEmployeeDetails();
+    this.formularioEmpleado.reset();
+  }
+  editarEmpleado(): void {
+    if (this.formularioEmpleado.valid) {
+      const empleadoEditado = {
+        id: this.employeeId,
+        nombre: this.formularioEmpleado.value.nombre,
+        apellido: this.formularioEmpleado.value.apellido,
+        email: this.formularioEmpleado.value.email,
+        password: this.formularioEmpleado.value.password,
+        departamento: this.formularioEmpleado.value.departamento,
+        sucursal: this.formularioEmpleado.value.sucursal,
+        rol: this.formularioEmpleado.value.rol,
+        telefonoMovil: this.formularioEmpleado.value.telefonoMovil
+      };
+  
+      this.employeeService.editEmployee(this.employeeId, empleadoEditado).subscribe(
+        (response: any) => {
+          console.log('Empleado editado correctamente', response);
+          // Puedes cerrar el modal y actualizar la lista de empleados o hacer cualquier otra acción necesaria
+          this.cerrarModal();
+          this.getEmployeeDetails();
+        },
+        (error: any) => {
+          console.error('Error al editar empleado:', error);
+        }
+      );
+    } else {
+      // Si el formulario no es válido, puedes mostrar un mensaje de error o realizar alguna otra acción
+      console.error('Formulario inválido');
+    }
+  }
+
+  obtenerDepartamento() {
+    this.employeeService.listDepartments().subscribe(
+      (response: any[]) => {
+        this.departamentos = response;
+      },
+      error => {
+        console.error('Error al obtener department:', error);
+      }
+    );
+  }
+
+  obtenerSucursales() {
+    this.employeeService.listBranchOffices().subscribe(
+      (response: any[]) => {
+        this.sucursales = response;
+      },
+      error => {
+        console.error('Error al obtener sucursales:', error);
+      }
+    );
   }
   confirmDelete(employee: any): void {
     const confirmacion = confirm(`¿Estás seguro de que quieres eliminar a ${employee.name}?`);
