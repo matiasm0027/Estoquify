3cea271d27e251b6fedc8a9037800d7d9eb8d231
matiasm0027 <matiasm0027@gmail.com>
2024-04-15 07:46:52 -0400

    CambiosSeguridadURL

--- FrontEnd/src/app/components/forgot-password/forgot-password.component.ts
+++ FrontEnd/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ApiRequestService } from 'src/app/services/api/api-request.service';
 
 @Component({
@@ -11,11 +11,12 @@ import { ApiRequestService } from 'src/app/services/api/api-request.service';
 export class ForgotPasswordComponent implements OnInit {
   changePasswordForm: FormGroup;
   errorMessage: string | null = null;
+  successMessage: string | null = null;
 
   constructor(
     private apiService: ApiRequestService,
     private formBuilder: FormBuilder,
-    //private router: Router
+    private router: Router
   ) {
     this.changePasswordForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]]
@@ -23,6 +24,9 @@ export class ForgotPasswordComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (localStorage.getItem('itsLoged')) {
+      this.router.navigate(['/home']);
+    }
   }
 
   submitResetPassword(): void {
@@ -31,13 +35,11 @@ export class ForgotPasswordComponent implements OnInit {
 
       this.apiService.resetPasswordRequest(emailValue).subscribe(
         (response) => {
-          console.log('Password reset email sent successfully:', response);
-          //this.router.navigate(['/login']);
+          this.successMessage = response.message;
+          this.errorMessage= "";
         },
         (error) => {
           this.errorMessage = error.error.error;
-          console.log(this.changePasswordForm.value.email)
-          console.error('Password reset error:', error);
         }
       );
     }
--- FrontEnd/src/app/services/usuarios/usuarios-control.service.ts
+++ FrontEnd/src/app/services/usuarios/usuarios-control.service.ts
@@ -24,6 +24,7 @@ export class UsuariosControlService {
 
   logout(): void {
     localStorage.removeItem('token');
+    localStorage.removeItem('itsLoged');
   }
 }
 
