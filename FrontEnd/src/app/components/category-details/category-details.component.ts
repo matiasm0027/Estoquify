5b7f2c2645987793116a2a085ad5e87183301281
Dario <98590843+DariograciaDEV@users.noreply.github.com>
2024-04-16 07:47:36 -0400

    viewCategoriesDetail

--- FrontEnd/src/app/components/category-details/category-details.component.ts
+++ FrontEnd/src/app/components/category-details/category-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiRequestService } from 'src/app/services/api/api-request.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-details',
@@ -9,9 +10,45 @@ import { ApiRequestService } from 'src/app/services/api/api-request.service';
 })
 export class CategoryDetailsComponent {
   categoryId!: number;
-  categoryDetails : any = {};
+  categoryDetails: any ={};
+  formularioEmpleado!: FormGroup;
 
-  constructor(private route: ActivatedRoute, private categoryService: ApiRequestService,) {}
+
+  constructor(
+    private fb: FormBuilder,
+    private categoryService: ApiRequestService,
+  ){}
+
+
+  ngOnInit(): void {
+    this.getCategoryDetails
+  }
+
+  initForm() {
+    this.formularioEmpleado = this.fb.group({
+      nombre: [this.categoryDetails.name, Validators.required],
+      apellido: [this.categoryDetails.last_name, Validators.required],
+      email: [this.categoryDetails.email, [Validators.required, Validators.email]],
+      departamento: [this.categoryDetails.department, Validators.required],
+      sucursal: [this.categoryDetails.branch_office, Validators.required],
+      rol: [this.categoryDetails.role, Validators.required],
+      telefonoMovil: [this.categoryDetails.phone_number]
+    });
+  }
+
+
+  getCategoryDetails(): void {
+    this.categoryService.getEmployeeDetails(this.categoryId)
+      .subscribe(
+        (employee: any) => {
+          this.categoryDetails = employee;
+          this.initForm(); // Initialize form after employee details are fetched
+        },
+        (error: any) => {
+          console.error('Error al obtener detalles del empleado:', error);
+        }
+      );
+  }
 
   
 }
