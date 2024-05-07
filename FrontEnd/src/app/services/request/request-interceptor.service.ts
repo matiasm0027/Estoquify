5430f49829c3fcf78d1f68a7b9f92d89985d5a46
Orma4705 <oriolmartincasas@gmail.com>
2024-05-07 08:39:12 -0400

    interceptor de solicitudes demasiadas

---
+++ FrontEnd/src/app/services/request/request-interceptor.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          if (event.status === 429) {
+            alert('Demasiadas solicitudes. Por favor, inténtelo de nuevo más tarde.');
+          }
+        }
+      })
+    );
+  }
+}
